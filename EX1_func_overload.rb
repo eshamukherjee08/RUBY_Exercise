6b1294440ea57f086f1520fd3cf80514c7f60868
Esha Mukherjee <eshadgr8@gmail.com>
2011-10-08 16:54:24 +0530

    ex1_func_overload added

---
+++ EX1_func_overload.rb
@@ -0,0 +1,23 @@
+def area_overload(*args)
+  if(args.size == 1)
+    area = 3.14*args[0]*args[0]
+    print "The area of CIRCLE is : #{area}\n"
+  elsif(args.size == 2)
+    area = args[0]*args[1]
+    print "The area of RECTANGLE is : #{area}\n"
+  elsif(args.size == 3)
+    a = args[0]
+    b = args[1]
+    c = args[2]
+    s = (a+b+c)/2
+    x = s * (s-a) * (s-b) * (s-c)
+    area = Math.sqrt(x).to_i
+    print "The area of TRIANGLE is : #{area}\n"
+  else
+    print "Sorry wrong number of arguments...cant parse!\n"
+  end
+end
+area_overload(1)
+area_overload(3,4)
+area_overload(3,4,5)
+area_overload()
