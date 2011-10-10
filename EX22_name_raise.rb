e6ddaa0734f79ff2f0353e4d24f493a349b58404
esha mukherjee <eshadgr8@gmail.com>
2011-10-10 18:22:55 +0530

    Edited EX22_name_raise.rb via GitHub

--- EX22_name_raise.rb
+++ EX22_name_raise.rb
@@ -1,29 +1,31 @@
 class Name
-  #attr_accessor :firstname, :lastname
-  def initialize()
-    
+  def initialize(firstname , *args)
+    begin
+      if (args.size == 0 || args.size > 1)
+        raise "exception_agrs"
+      else
+        @firstname = firstname
+        lastname = args[0]
+        @lastname = lastname
+      end
+    rescue
+      abort "Handling WRONG input exception : #{$!}"
+    end
   end
-  def printt(firstname,*args)
-    @firstname = firstname
-    lastname = args[0]
-    @lastname = lastname
+  def to_s
     reg = /^[A-Z]/
     begin
-      if !(firstname =~ reg)
+      if !(@firstname =~ reg)
         raise "exception_caps"
-      elsif (args.size == 0 || args.size > 1)
-        raise "exception_agrs"
       else
         puts "FirstName : #{@firstname} \n& \nLastName : #{@lastname}"
       end
     rescue
       if $!.to_s == "exception_caps"
         puts "Handling SMALL CASE input exception : #{$!}"
-      else
-        puts "Handling WRONG input exception : #{$!}"
       end
     end
   end
 end
-p1 = Name.new
-p1.printt("Esha","Mukherjee")
+p1 = Name.new("Esha","Mukherjee")
+puts p1
